c32f844799f19ab4b25035be8864548083610493
Chris leung <ckleung@cpp.edu>
2020-03-18 23:48:08 -0700

    FIXED

--- main.cpp
+++ main.cpp
@@ -1,103 +1,141 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sstream>
 #include <string.h>
 #include "AESEncrypt128.h"
 
-void pkcs5(unsigned char dataBlock[], int buffByte);
-
-
-void keyInput(unsigned char myactualyKey[], unsigned char userString[], int userStringSize);
-
 using namespace std;
+//B2 F6 CF D2 1D 00 AF F9 16 9A 1D B8 0D d8 40 04
+//B2F6CFD21D00AFF9169A1DB80Dd84004
+
+void pkcs5(t_uint8 dataBlock[], int buffByte);
+bool isHex(char character);
+bool checkKey(string* user);
+string clearWhiteSpace(string* userString);
+void initAESByteArray(t_uint8 * keyArr, string* userString);
 
 int main(int argc, char *argv[]) {
-    stringstream iss;
-    unsigned char dataBuffer[BUFFSIZE]; // input buffer
-    unsigned char myKeyStr[] = {"B2 F6 CF D2 1D 00 AF F9 16 9A 1D B8 0D d8 40 04"};
-    string hexkey = "B2 F6 CF D2 1D 00 AF F9 16 9A 1D B8 0D d8 40 04";
-    iss << hex << hexkey;
-    unsigned char myActualKey[16] = {0};
-    unsigned char test1 = 0;
-    for(int i = 0; i < sizeof(myKeyStr); i++)
-    {
-        test1 = (unsigned char) strtoul(hexkey, NULL, 16);
-        myActualKey[i] = test1;
-    }
+    t_uint8 dataBuffer[BUFFSIZE];                                       /* Input buffer from file */
+    t_uint8 aesKey[16] = {0};                                           /* Holds key from user byte value */
+    t_uint8 * aesKey_Ptr = aesKey;                                      /* Pointer to aes key array */
+    string userInputKey = "";                                           /* User input string used for parser */
+    bool isValidKey = false;                                            /* Used to check valid key */
+    char userChoice = '0';
 
-//    int c = 0;
-//    int index = 0;
-//    while(iss >> hex >> c)
-//    {
-//        myActualKey[index] = c;
-//        index++;
-//    }
     cout << "Hello world" << endl;
 
-    if (argc <= 1) {
+    if (argc <= 1) {                                                    /* If number of arguments less than req. */
         printf("No file path specified, exiting program");
+        exit(-1);                                                /* Exit with status code -1 */
+    }
+
+    const char *source = argv[1];                                       /* Take in argv[1] for file*/
+    FILE* src = fopen(source,"rb");                               /* Try to open and read binaries of file */
+    if(src == NULL)                                                     /* If source cannot be opened then close */
+    {
+        perror("Cannot open file, Closing Program");
+        exit(-1);                                                /* Exit with status code -1 */
+    }
+
+    do {
+        cout << "Please enter your key:";                               /* Prompt user for key */
+        getline(cin, userInputKey);                             /* Get entire line from user */
+
+        userInputKey = clearWhiteSpace(&userInputKey);                  /* Function to remove white spaces */
+
+        isValidKey = checkKey(&userInputKey);                           /* Check to see if key has all hex */
+
+        if(!isValidKey){                                                /* if key is incorrect ask user for another */
+            cout << "\nKey contains error" << endl;
+            cout << "Would you like to enter a new key (y/n)? : ";
+            cin >> userChoice;
+            cin.ignore();
+        }
+    }while(!isValidKey && (userChoice == 'y'|| userChoice == 'Y'));     /* do-while user wants to input new keys */
+    if(!isValidKey){                                                    /* if still invalid key exit program */
+        cout << "Exiting program, cannot use key from user" << endl;
         exit(0);
     }
 
-    const char *source = argv[1];   //input source file
-    FILE* src = fopen(source,"rb");
-    if(src == NULL)
+    initAESByteArray(aesKey_Ptr, &userInputKey);                        /* Initialize byte array with hex string */
+
+    memset(dataBuffer, 0, sizeof(dataBuffer));                          /* Wipe out garbage data in data buffer */
+
+    fseek(src,0, SEEK_END);                                             /* fseek to count # bytes in file */
+    int fileSize = ftell(src);                                          /* Determine file size*/
+    rewind(src);                                                        /* Reset file pointer*/
+    bool extraPadBlock = fileSize%16 == 0;                              /* Determine if file is multiple of 16 bytes*/
+
+    printf("Filesize is %d bytes\n",fileSize);                          /* Printout for file size*/
+    int readFromBuffer = 0;                                             /* Counter for data buffer*/
+
+    while(!feof(src))                                                   /* While file not end of file*/
     {
-        perror("Cannot open file, Closing Program");
-        exit(-1);
-    }
-
-    unsigned char userString[50] = {0};
-    cout << "Please enter your key:";
-    cin.getline((char*)userString,sizeof(userString));
-
-    keyInput(myActualKey, userString, sizeof(userString));
-    for(int i = 0; i < sizeof(myActualKey); i++)
-        printf("%X ", myActualKey[i]);
-
-    memset(dataBuffer, 0, sizeof(dataBuffer)); //initialize buffer with 0
-
-    fseek(src,0, SEEK_END);
-    int fileSize = ftell(src);
-    rewind(src);
-    bool extraPadBlock = fileSize%16 == 0;
-
-    printf("Filesize is %d bytes\n",fileSize);
-    int readFromBuffer = 0;
-
-    while(!feof(src))
-    {
-        readFromBuffer = fread(dataBuffer, sizeof(unsigned char), BUFFSIZE, src);
-        if(readFromBuffer <= BUFFSIZE)
-            pkcs5(dataBuffer, readFromBuffer);
+        readFromBuffer = fread(dataBuffer, sizeof(t_uint8), BUFFSIZE, src); /* Read 16 bytes to buffer */
+        if(readFromBuffer <= BUFFSIZE)                                  /* If bytes read less than buffer size, pad */
+            pkcs5(dataBuffer, readFromBuffer);                          /* perform pkcs5 on data buffer to pad */
 
         encryptBlock(dataBuffer);
+
+        //fwrite(dataBuffer, sizeof(char), BUFFSIZE, dst)               /* Write out encrypted to file */
     }
-
-    for(int i = 0; i < sizeof(dataBuffer); i++)
-        printf("%X ",dataBuffer[i]);
     fclose(src);
-
     return 0;
 }
 
-void keyInput(unsigned char myactualyKey[], unsigned char userString[], int userStringSize) {
-    int keyIndex = 0;
-    for(int strIndex = 0; strIndex < userStringSize; strIndex++)
+void initAESByteArray(t_uint8 * keyArr, string* userString)
+{
+    string copiedString = *userString;
+    int byteArrIndex = 0;
+    string byteString = "";
+    int starting = 0;
+    for(int i = 0; i < copiedString.length(); i+=2)
     {
-        if(userString[strIndex] == ' ' || userString[strIndex] == NULL)
-            continue;
-        else {
-            myactualyKey[keyIndex] = toupper(userString[strIndex]);
-            keyIndex++;
-        }
+        starting = i;
+        byteString = copiedString.substr(starting, 2);
+        keyArr[byteArrIndex++] = stoi(byteString, nullptr, 16);
+        byteString.clear();
     }
 }
 
-void pkcs5(unsigned char dataBlock[], int numBytes) {
+bool checkKey(string* user){
+    bool chkSuccess = false;
+    string copiedString = *user;
+    int strLength = copiedString.length();
+    if(strLength != 32) return chkSuccess;
+    for(int index = 0; index < strLength; index++)
+    {
+        if(isHex(copiedString[index]))
+            continue;
+        else return false;
+    }
+    return true;
+}
+
+string clearWhiteSpace(string* userString){
+    string copiedString = *userString;
+    string newString = "";
+    for(int strIndex = 0; strIndex < copiedString.length(); strIndex++)
+    {
+        if(copiedString[strIndex] == ' ' || copiedString[strIndex] == NULL)
+            continue;
+        else{
+            newString += toupper(copiedString[strIndex]);
+        }
+    }
+    return newString;
+}
+
+bool isHex(char character){
+    bool checkHex = (character >= 'a' && character <= 'f')||
+            (character >= 'A' && character <= 'F')||
+            (character >= '0' && character <= '9');
+    return checkHex;
+}
+
+void pkcs5(t_uint8 dataBlock[], int numBytes) {
     int i = numBytes;
-    unsigned char difference = BUFFSIZE - numBytes;
+    t_uint8 difference = BUFFSIZE - numBytes;
     for(i; i < BUFFSIZE; i++){
         dataBlock[i] = difference;
     }
