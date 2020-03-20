9c57d3c162e000530111b916f678c0593a893450
Chris leung <ckleung@cpp.edu>
2020-03-20 02:51:28 -0700

    night

--- main.cpp
+++ main.cpp
@@ -11,40 +11,29 @@
 #include <string.h>
 #include "AESEncrypt128.h"
 
-using namespace std;
+//using namespace std;
 //B2 F6 CF D2 1D 00 AF F9 16 9A 1D B8 0D d8 40 04
 //B2F6CFD21D00AFF9169A1DB80Dd84004
 //BEEF4DADBEEF4DADBEEF4DADBEEF4DAD
 
 void pkcs5(t_uint8 dataBlock[], int buffByte);
 bool isHex(char character);
-bool checkKey(string* user);
-string clearWhiteSpace(string* userString);
-void initAESByteArray(t_uint8 * keyArr, string* userString);
+bool checkKey(std::string* user);
+std::string clearWhiteSpace(std::string* userString);
+void initAESByteArray(t_uint8 * keyArr, std::string* userString);
 void gBox(t_uint8* gbox, t_uint8* keyptr, unsigned int rc);
 void xorFunction(t_uint8* arr1, t_uint8* arr2, t_uint8* currentBytePtr);
-/**************************
- * for packaging later on *
- **************************/
 t_uint8* genKeySchedule(t_uint8* key_ptr);
-/**************************
- * for packaging later on *
- **************************/
 
 int main(int argc, char *argv[]) {
     t_uint8 dataBuffer[BUFFSIZE];                                       /* Input buffer from file */
     t_uint8 aesKey[16] = {0};                                           /* Holds key from user byte value */
     t_uint8 * aesKey_Ptr = aesKey;                                      /* Pointer to aes key array */
-    string userInputKey = "";                                           /* User input string used for parser */
+    std::string userInputKey = "";                                      /* User input string used for parser */
     bool isValidKey = false;                                            /* Used to check valid key */
     char userChoice = '0';
-#if 0
-    t_uint8 testing[4] = {0xBE, 0xEF, 0x4D, 0xAD};
-    t_uint8* testPtr = testing;
-    gBox(testPtr, 1);
 
-#endif
-    cout << "Hello world" << endl;
+    std::cout << "Hello world" << std::endl;
 
     if (argc <= 1) {                                                    /* If number of arguments less than req. */
         printf("No file path specified, exiting program");
@@ -60,28 +49,38 @@ int main(int argc, char *argv[]) {
     }
 
     do {
-        cout << "Please enter your key:";                               /* Prompt user for key */
-        getline(cin, userInputKey);                             /* Get entire line from user */
+        std::cout << "Please enter your key:";                          /* Prompt user for key */
+        getline(std::cin, userInputKey);                        /* Get entire line from user */
 
         userInputKey = clearWhiteSpace(&userInputKey);                  /* Function to remove white spaces */
 
         isValidKey = checkKey(&userInputKey);                           /* Check to see if key has all hex */
 
         if(!isValidKey){                                                /* if key is incorrect ask user for another */
-            cout << "\nKey contains error" << endl;
-            cout << "Would you like to enter a new key (y/n)? : ";
-            cin >> userChoice;
-            cin.ignore();
+            std::cout << "\nKey contains error" << std::endl;
+            std::cout << "Would you like to enter a new key (y/n)? : ";
+            std::cin >> userChoice;
+            std::cin.ignore();
         }
     }while(!isValidKey && (userChoice == 'y'|| userChoice == 'Y'));     /* do-while user wants to input new keys */
     if(!isValidKey){                                                    /* if still invalid key exit program */
-        cout << "Exiting program, cannot use key specified by user" << endl;
+        std::cout << "Exiting program, cannot use key specified by user" << std::endl;
         exit(0);
     }
 
 
     initAESByteArray(aesKey_Ptr, &userInputKey);                        /* Initialize byte array with hex string */
-    genKeySchedule(aesKey_Ptr);
+    t_uint8* keys = genKeySchedule(aesKey_Ptr);
+    for(int i = 0; i < 11; i++)
+    {
+        printf("Key %d:",i);
+        for(int j = 0; j < 16; j++)
+        {
+            printf("%02X",(keys[j]));
+        }
+        keys+=16;
+        std::cout<<std::endl;
+    }
 
     memset(dataBuffer, 0, sizeof(dataBuffer));                          /* Wipe out garbage data in data buffer */
 
@@ -99,7 +98,7 @@ int main(int argc, char *argv[]) {
         if(readFromBuffer <= BUFFSIZE)                                  /* If bytes read less than buffer size, pad */
             pkcs5(dataBuffer, readFromBuffer);                          /* perform pkcs5 on data buffer to pad */
 
-        encryptBlock(dataBuffer);
+        //encryptBlock(dataBuffer);
 
         //fwrite(dataBuffer, sizeof(char), BUFFSIZE, dst)               /* Write out encrypted to file */
     }
@@ -107,60 +106,60 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void initAESByteArray(t_uint8 * keyArr, string* userString)
+void initAESByteArray(t_uint8 * keyArr, std::string* userString)
 {
-    string copiedString = *userString;
-    int byteArrIndex = 0;
-    string byteString = "";
-    int starting = 0;
+    std::string copiedString = *userString;                             /* Copy string */
+    int byteArrIndex = 0;                                               /* initialize byte array index to 0*/
+    std::string byteString = "";                                        /* Initial byte string is empty*/
+    int starting = 0;                                                   /* Starting index to a length*/
     for(int i = 0; i < copiedString.length(); i+=2)
     {
-        starting = i;
-        byteString = copiedString.substr(starting, 2);
-        keyArr[byteArrIndex++] = stoi(byteString, nullptr, 16);
+        starting = i;                                                   /* Starting index to keep track of chars*/
+        byteString = copiedString.substr(starting, 2);               /* copy 2 chars into byte string*/
+        keyArr[byteArrIndex++] = stoi(byteString, nullptr, 16); /* convert string to int and store in array*/
         byteString.clear();
     }
 }
 
-bool checkKey(string* user){
-    bool chkSuccess = false;
-    string copiedString = *user;
-    int strLength = copiedString.length();
-    if(strLength != 32) return chkSuccess;
-    for(int index = 0; index < strLength; index++)
+bool checkKey(std::string* user){
+    bool chkSuccess = false;                                             /* Assign bool return value to false */
+    std::string copiedString = *user;                                    /* Create a copied string from user */
+    int strLength = copiedString.length();                               /* Used for finding string length */
+    if(strLength != 32) return chkSuccess;                               /* if str is less than 32 chars, return false*/
+    for(int index = 0; index < strLength; index++)                       /* Parse entire string for hex chars*/
     {
-        if(isHex(copiedString[index]))
+        if(isHex(copiedString[index]))                                   /* If hex char found then continue */
             continue;
-        else return false;
+        else return false;                                               /* Else return false bool flag*/
     }
-    return true;
+    return true;                                                         /* If nothing found wrong return true */
 }
 
-string clearWhiteSpace(string* userString){
-    string copiedString = *userString;
-    string newString = "";
-    for(int strIndex = 0; strIndex < copiedString.length(); strIndex++)
+std::string clearWhiteSpace(std::string* userString){                    /* This Function strips white space */
+    std::string copiedString = *userString;                              /* Copy local string var inside function */
+    std::string newString = "";                                          /* Init. new empty string */
+    for(int strIndex = 0; strIndex < copiedString.length(); strIndex++)  /* Begin parsing */
     {
-        if(copiedString[strIndex] == ' ' || copiedString[strIndex] == '\0')
+        if(copiedString[strIndex] == ' ' || copiedString[strIndex] == '\0') /* If found space or null continue */
             continue;
         else{
-            newString += toupper(copiedString[strIndex]);
+            newString += toupper(copiedString[strIndex]);			      /* Else copy char as upper case */
         }
     }
-    return newString;
+    return newString;                                                     /* Return new string */
 }
 
-bool isHex(char character){
-    bool checkHex = (character >= 'a' && character <= 'f')||
-            (character >= 'A' && character <= 'F')||
-            (character >= '0' && character <= '9');
-    return checkHex;
+bool isHex(char character){                                               /* This function determines hex chars*/
+    bool checkHex = (character >= 'a' && character <= 'f')||              /* Return boolean if a hex char */
+                    (character >= 'A' && character <= 'F')||
+                    (character >= '0' && character <= '9');
+    return checkHex;                                                      /* Return boolean */
 }
 
-void pkcs5(t_uint8 dataBlock[], int numBytes) {
-    int i = numBytes;
-    t_uint8 difference = BUFFSIZE - numBytes;
-    for(i; i < BUFFSIZE; i++){
+void pkcs5(t_uint8 dataBlock[], int numBytes) {                           /* This function pads block of plaintext */
+    int i = numBytes;                                                     /* assign i to number of bytes read */
+    t_uint8 difference = BUFFSIZE - numBytes;                             /* Take difference from buffersize */
+    for(i; i < BUFFSIZE; i++){                                            /* For loop to pad the end of data block */
         dataBlock[i] = difference;
     }
 }
@@ -170,93 +169,66 @@ void pkcs5(t_uint8 dataBlock[], int numBytes) {
  * For data packaging later on    *
  * ********************************/
 
-t_uint8* genKeySchedule(t_uint8* key_ptr)
+t_uint8* genKeySchedule(t_uint8* key_ptr)                                 /* This Function generates the AES keys*/
 {
-
-    t_uint8 keyArr_bytes[176] = {0};
-    t_uint8* currentBytePtr = keyArr_bytes;
-    t_uint8* key_ptr_ptr = key_ptr;
-    for(int i = 0; i < 16; i++){                     /* copy over first 4 words of key */
-        *currentBytePtr++ = *key_ptr_ptr++;
+    static t_uint8 keyArr_bytes[176] = {0};                               /* Init 176 byte unsigned char array */
+    t_uint8* currentBytePtr = keyArr_bytes;                               /* Pointer to reference the array */
+    t_uint8* key_ptr_ptr = key_ptr;										  /* Used for referencing the key passed in */
+    for(int i = 0; i < 16; i++){                     				      /* Copy over first 4 words of key */
+        *currentBytePtr++ = *key_ptr_ptr++;								  /* Copy over key to words 0 - word 3 */
     }
 
-    t_uint8* w1 = keyArr_bytes;
-    t_uint8* w2 = w1 + 4;
-    t_uint8* w3 = w2 + 4;
-    t_uint8* w4 = w3 + 4;
+    t_uint8* w1 = keyArr_bytes;											  /* Word 1 pointer */
+    t_uint8* w2 = w1 + 4;                                                 /* Word 2 pointer (add 4 bytes to word 1) */
+    t_uint8* w3 = w2 + 4;                                                 /* Word 3 pointer (add 4 bytes to word 2) */
+    t_uint8* w4 = w3 + 4;                                                 /* Word 4 pointer (add 4 bytes to word 3) */
 
-    for(int i = 1; i < 11; i ++)
+    for(int i = 1; i < 11; i ++)                                          /* For loop to iterate over key generation */
     {
-        t_uint8 gBoxReturn[4] = {0};
-        gBox(&gBoxReturn[0],w4,i);
-        xorFunction(&gBoxReturn[0], w1, currentBytePtr);
-        currentBytePtr += 4;
-        w1 += 16;
-        xorFunction(w1, w2, currentBytePtr);
-        (currentBytePtr += 4);
-        w2+= 16;
-        xorFunction(w2,w3,currentBytePtr);
-        (currentBytePtr += 4);
-        w3+=16;
-        xorFunction(w3,w4,currentBytePtr);
-        (currentBytePtr += 4);
-        w4+=16;
-
+        t_uint8 gBoxReturn[4] = {0};                                      /* Holds 4 bytes from gBox output */
+        gBox(&gBoxReturn[0],w4,i);                                        /* Calculate gBox using word 4 and current round */
+        xorFunction(&gBoxReturn[0], w1, currentBytePtr);                  /* gbox ^ first word of current key then write to key arr */
+        currentBytePtr += 4;                                              /* Increment the byte pointer by 4 bytes to reference next word */
+        w1 += 16;                                                         /* Increment word by 16 bytes to refrence new word */
+        xorFunction(w1, w2, currentBytePtr);                              /* First of key ^ second word of key, then write to key arr */
+        (currentBytePtr += 4);                                            /* Increment byte pointer by 4 to refrence next word */
+        w2+= 16;                                                          /* Increment second word of key by 16 bytes */
+        xorFunction(w2,w3,currentBytePtr);                                /* Second word of key ^ third word of key then write to key arr */
+        (currentBytePtr += 4);                                            /* Increment byte pointer by 4 bytes */
+        w3+=16;                                                           /* Increment 3rd word pointer by 16 bytes */
+        xorFunction(w3,w4,currentBytePtr);                                /* Third word of key ^ fourth word of key then write to key arr */
+        (currentBytePtr += 4);                                            /* Increment byte pointer by 4 bytes */
+        w4+=16;                                                           /* Increment 4th word by 16 bytes */
     }
-    return &keyArr_bytes[0];
-
+    return &keyArr_bytes[0];                                              /* Return all the keys back to caller */
 }
 
-void gBox(t_uint8* gbox, t_uint8* keyptr, unsigned int roundKey)
+void gBox(t_uint8* gbox, t_uint8* keyptr, unsigned int roundKey)          /* This function calculates the gbox for AES*/
 {
-    gbox[0] = *keyptr;
-    gbox[1] = *(keyptr + 1);
-    gbox[2] = *(keyptr + 2);
-    gbox[3] = *(keyptr + 3);
-    t_uint8 temp = gbox[0];
+    gbox[0] = *keyptr;                                                    /* First gBox Index = first byte of word 4 in key */
+    gbox[1] = *(keyptr + 1);                                              /* Second gBox Index = second byte of word 4 in key */
+    gbox[2] = *(keyptr + 2);                                              /* Third gBox Index = Third byte of word 4 in key */
+    gbox[3] = *(keyptr + 3);                                              /* Forth gbox Index = forth byte in word 4 in key */
+    t_uint8 temp = gbox[0];                                               /* Temp unsigned char variable to hold first gbox index */
 
-    gbox[0] = gbox[1];
-    gbox[1] = gbox[2];
-    gbox[2] = gbox[3];
-    gbox[3] = temp;
+    gbox[0] = gbox[1];                                                    /* Left cyclic byte shift */
+    gbox[1] = gbox[2];                                                    /* Left cyclic byte shift */
+    gbox[2] = gbox[3];                                                    /* Left cyclic byte shift */
+    gbox[3] = temp;                                                       /* Index 4 = first byte in original gbox */
 
-    for(int i = 0; i < 4; i++)
-    {
-        gbox[i] = getSBox(gbox[i]);
-    }
+    for(int i = 0; i < 4; i++){                                           /* For loop to do byte substituion */
+        gbox[i] = getSBox(gbox[i]);                                       /* Used loopup table */
+    }                                                                     /**/
 
-    gbox[0] ^= rcon[roundKey];
-
-//    t_uint8* current = keyptr;
-//    t_uint8* next = keyptr + sizeof(unsigned char);
-//    t_uint8 temp = *keyptr;
-//
-//    for(int i = 0; i < 3; i++)
-//    {
-//        *current = *next;
-//        next++;
-//        current++;
-//    }
-//    *current = temp;
-//
-//    current -= 3*sizeof(unsigned char);
-//
-//    for(int i = 0; i < 4;i++)
-//    {
-//        *current = getSBox(*current);
-//        current++;
-//    }
-//
-//    current -= 4*sizeof(unsigned char);
-//    *current ^= rcon[roundKey+1];
+    gbox[0] ^= rcon[roundKey];                                            /* XOR byte in index 0 with round constant */
 }
 
-void xorFunction(t_uint8* arr1, t_uint8* arr2, t_uint8* currentBytePtr)
+void xorFunction(t_uint8* arr1, t_uint8* arr2, t_uint8* currentBytePtr)   /* This function calculates XOR of 2 pointers then stores */
 {
-    t_uint8* arrPtr1 = arr1;
-    t_uint8* arrPtr2 = arr2;
-    for(int i = 0; i < 4; i++){
-        *currentBytePtr = (*arrPtr1++) ^ (*arrPtr2++);
-        currentBytePtr++;
+    t_uint8* arrPtr1 = arr1;                                              /* Temp pointer to array 1 */
+    t_uint8* arrPtr2 = arr2;                                              /* Temp pointer to array 2 */
+    for(int i = 0; i < 4; i++){                                           /* for loop to xor every individual byte */
+        *currentBytePtr = (*arrPtr1++) ^ (*arrPtr2++);                    /* Need to derefrence pointer first then perform xor operation */
+        currentBytePtr++;                                                 /* Increment pointer to key array */
     }
 }
